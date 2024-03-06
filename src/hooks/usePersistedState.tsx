1870eef78094fdaf0060aa6ec9d606bfbad10c81
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-03-06 08:50:11 -0300

    Update usePersistedState.tsx

--- src/hooks/usePersistedState.tsx
+++ src/hooks/usePersistedState.tsx
@@ -12,7 +12,7 @@ function usePersistedState<T>(key: string, initialState: T): Response<T> {
 
     let storageValue
 
-    if (typeof window !== undefined) {
+    if (window !== undefined) {
       storageValue = localStorage.getItem(key);
 
       if (storageValue) {
@@ -26,7 +26,7 @@ function usePersistedState<T>(key: string, initialState: T): Response<T> {
   });
 
   useEffect(() => {
-    if (typeof window !== undefined) {
+    if (window !== undefined) {
       localStorage.setItem(key, JSON.stringify(state));
     }
 
