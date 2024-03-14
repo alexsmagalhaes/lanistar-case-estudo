22358955b3ae7b1b55d567004f898acb16283b49
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-03-14 17:09:53 -0300

    style(remove spaces and organize)

--- src/components/section/Navbar.tsx
+++ src/components/section/Navbar.tsx
@@ -27,12 +27,12 @@ const pagesRef = [
 ]
 
 export default function Navbar(): ReactNode {
-   
+
    return (
       <nav className="py-6 max-h-24 bg-white bg-opacity-90 sticky top-0 border-b-neutral-gray-600 border-b border-opacity-30 z-40 backdrop-blur">
          <ContainerGrid className="flex items-center justify-between">
             <Link href='/' className="hover:opacity-85 transition duration-300 ease-in-out">
-               <Image src={Logo} alt={"logo lenistar"} width={176} priority/>
+               <Image src={Logo} alt={"logo lenistar"} width={176} priority />
             </Link>
 
             <div className="flex gap-24">
@@ -55,12 +55,12 @@ export default function Navbar(): ReactNode {
                <DropDownLanguage />
 
                <Button.Root type="ghost">
-               <svg width="32" height="24" viewBox="0 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="1" y="1" width="30" height="22" rx="1" stroke="currentColor" strokeWidth="2" />
-                  <rect x="4.36353" y="4.36353" width="5.81818" height="4.36364" rx="2" fill="currentColor" />
-               </svg>
-               Botão de ação
-            </Button.Root>
+                  <svg width="32" height="24" viewBox="0 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                     <rect x="1" y="1" width="30" height="22" rx="1" stroke="currentColor" strokeWidth="2" />
+                     <rect x="4.36353" y="4.36353" width="5.81818" height="4.36364" rx="2" fill="currentColor" />
+                  </svg>
+                  Botão de ação
+               </Button.Root>
             </div>
          </ContainerGrid>
       </nav>
