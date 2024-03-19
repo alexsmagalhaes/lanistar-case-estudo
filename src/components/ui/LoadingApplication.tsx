5dca5a4adfefc501e4ffcc89a56a3d3c5e5f210e
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-03-19 10:06:13 -0300

    feat(add a loading screen)

---
+++ src/components/ui/LoadingApplication.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import usePageLoaded from '@/hooks/usePageLoad'
+
+//assets
+import LoadingImage from '@/assets/brand/star-logo.svg'
+
+export default function LoadingApplication() {
+   const [loadingState, setLoadingState] = useState<boolean>(true)
+   const loaded = usePageLoaded();
+
+   useEffect(() => {
+      loaded === true && setLoadingState(false)
+   }, [loaded]);
+
+   return (
+      <div data-loading={loadingState} className="bg-white z-50 w-screen h-screen fixed flex items-center transition-all duration-500 bg-opacity-0 delay-300 ease-in-out pointer-events-none justify-center pb-12 data-[loading=true]:pointer-events-auto data-[loading=true]:bg-opacity-100 data-[loading=true]:backdrop-blur">
+         <Image
+            data-loading={loadingState}
+            className='data-[loading=true]:opacity-100 opacity-0 transition-all duration-500 ease-in-out'
+            src={LoadingImage}
+            alt='loading image'
+            width={72}
+         />
+      </div>
+   )
+}
