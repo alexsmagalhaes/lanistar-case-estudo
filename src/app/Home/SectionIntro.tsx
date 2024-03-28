32360f071b6bc3a8875ed72734cbb4d964386618
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-03-28 10:31:53 -0300

    update(add mobile version)

--- src/app/Home/SectionIntro.tsx
+++ src/app/Home/SectionIntro.tsx
@@ -12,14 +12,14 @@ const checksContent = [
 export default function SectionIntro(): ReactNode {
    return (
       <section>
-         <ContainerGrid className="border-b border-neutral-gray-600 border-opacity-45 py-28">
-            <div className="flex justify-between items-start gap-8 max-w-[65.5rem]">
-               <h2 className="text-[3.5rem] uppercase font-light text-neutral-gray-400 leading-none">
+         <ContainerGrid className="border-b border-neutral-gray-600 border-opacity-45 py-[5.625rem] @laptop:py-28">
+            <div className="flex justify-between items-start flex-col @laptop:flex-row gap-10 @laptop:gap-8 max-w-[65.5rem]">
+               <h2 className="text-[2rem] @laptop:text-[3.5rem] uppercase font-light text-neutral-gray-400 leading-none">
                   LANISTAR <br />
-                  <span className="text-neutral-gray-300">
+                  <span className="text-neutral-gray-200">
                      PAYMENT CARD</span>
                </h2>
-               <div className="flex gap-6 flex-col items-start max-w-96">
+               <div className="flex gap-8 @laptop:gap-6 flex-col items-start @laptop:max-w-96">
                   {
                      checksContent.map((text: string, index: number) => {
                         return (
