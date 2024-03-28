fe6efe84d9258d95dae1eb063038e75c93b23f6e
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-03-28 10:32:33 -0300

    fix(change text orientation)

--- src/components/ui/Button/ButtonRoot.tsx
+++ src/components/ui/Button/ButtonRoot.tsx
@@ -6,10 +6,10 @@ const ButtonVariants = tv({
    base: 'font-semibold uppercase ring-0 transition-all duration-300 ring-opacity-0 ease-in-out rounded-md flex items-center gap-4',
    variants: {
       size: {
-         default: 'text-base/snug px-8 py-4',
+         default: 'text-base/snug px-8 py-4 flex items-center',
       },
       type: {
-         primary: 'bg-brand-blue-default hover:bg-brand-blue-dark hover:ring-4 hover:ring-opacity-20 border border-brand-blue-default hover:border-brand-blue-dark ring-brand-blue-default text-white',
+         primary: 'bg-brand-blue-default hover:bg-brand-blue-dark hover:ring-4 hover:ring-opacity-20 border border-brand-blue-default hover:border-brand-blue-dark ring-brand-blue-default text-white justify-center',
          ghost: 'text-neutral-gray-200 [&_svg]:text-brand-blue-default [&_svg]:transition [&_svg]:ease-in-out [&_svg]:duration-300 [&_svg]:hover:text-brand-blue-dark py-0 px-0'
       }
    },
