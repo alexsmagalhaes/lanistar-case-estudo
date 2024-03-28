4e3b400c198cc1b9cb9ff16aa51c295379bf9553
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-03-28 11:15:34 -0300

    fix(update animation duration)

--- src/components/ui/Modal/ModalRoot.tsx
+++ src/components/ui/Modal/ModalRoot.tsx
@@ -10,7 +10,7 @@ export default function ModalRoot({ children }: { children: ReactNode }): ReactN
    return (
       <aside
          data-modal-active={modalActive}
-         className="fixed z-50 items-end @laptop:items-center justify-center @laptop:p-page-padding top-0 bottom-0 right-0 left-0 flex max-h-dvh scroll-auto transition ease-in-out duration-200 data-[modal-active=false]:opacity-0 data-[modal-active=false]:pointer-events-none"
+         className="fixed z-50 items-end @laptop:items-center justify-center @laptop:p-page-padding top-0 bottom-0 right-0 left-0 flex max-h-dvh scroll-auto transition ease-in-out duration-300 data-[modal-active=false]:opacity-0 data-[modal-active=false]:pointer-events-none"
       >
          <div
             className="bg-black bg-opacity-65 fixed top-0 bottom-0 right-0 left-0"
